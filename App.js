c4f811691cf39baaf6644a2622bbb2b439f8c5db
Siphe23 <siphesihlemzamane@gmai.com>
2024-12-08 23:22:11 +0200

    commit

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from './components/Login'; // Assuming you have this component
-import Signup from './components/Signup'; // Assuming you have this component
-import Home from './components/Home'; // Your Home component
-import Profile from './components/Profile'; // Your Profile component
+import Login from './components/Login'; 
+import Signup from './components/Signup'; 
+import Home from './components/Home'; 
+import Profile from './components/Profile'; 
+import {ImageBackground } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+        <ImageBackground
+      source={require('./images/mic.jpg')}
+      style={styles.backgroundImage}
+    ></ImageBackground>
       <Stack.Navigator>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Signup" component={Signup} />
--- styles/colors.js
+++ styles/colors.js
@@ -1,9 +1,9 @@
 export const colors = {
-    background: '#f4f4f5', // Light zinc
-    text: '#1f2937',       // Dark zinc
-    button: '#3f3f46',     // Medium zinc
-    icon: '#64748b',       // Blue-gray zinc
-    error: '#ef4444',      // Red for errors
-    success: '#10b981',    // Green for success
-  };
-  
+  background: '#f4f4f5', 
+  text: '#1f2937',       
+  button: '#3f3f46',     
+  icon: '#64748b',       
+  error: '#ef4444',      
+  success: '#10b981',    
+  backgroundImage: 'u)' 
+};
